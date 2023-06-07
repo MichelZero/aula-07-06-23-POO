fb750edd9fcfe36dd25087b768a2c8fdeeebae4b
Michel <106674127+MichelZero@users.noreply.github.com>
2023-06-07 08:48:41 -0300

    up

---
+++ calcIMC.py
@@ -0,0 +1,20 @@
+#
+#
+# autor: Michel
+#
+# data: 07/06/2023
+
+# criar a classe CalculadoraIMC.
+class CalculadoraIMC():
+  altura = None
+  peso = None
+  
+  def setAltura(self, valor):
+    self.altura = valor
+    
+  def setPeso(self, valor):
+    self.peso = valor
+    
+  def IMC(self):
+    resultado = self.peso / self.altura ** 2
+    return resultado
