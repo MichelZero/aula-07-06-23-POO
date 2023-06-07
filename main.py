30b7959f43fea5af4dd36900d175943191b89ffe
Michel <106674127+MichelZero@users.noreply.github.com>
2023-06-07 09:10:19 -0300

    up

--- main.py
+++ main.py
@@ -4,15 +4,32 @@
 #
 # data: 07/06/2023
 
-# importando a classe Pessoa.
-from pessoa import Pessoa as P
+# importando a classe CalculadoraIMC
+from calcIMC import CalculadoraIMC as CIMC
 
-# criando os objetos do tipo pessoa
-p1 = P()
+# criando o objeto CalculadoraIMC
+calc1 = CIMC()
 
-# chamando os métodos
-print(f"nome original é {p1.nome}")
-p1.trocaNome("Davi")
-print(f"novo nome é {p1.nome}")
-print(f"novo nome é {p1.informeNome()}")
+# entrada de dados
+peso = int(input("informe o peso: "))
+altura = float(input("informe a altura: "))
+#
+calc1.setPeso(peso)
+calc1.setAltura(altura)
+#print(calc1.IMC())
 
+# https://endocrinologiacuritiba.com.br/wp-content/uploads/2012/02/tabela_imc.jpg
+if calc1.IMC() < 18.5:
+  print("abaixo do peso normal!")
+elif calc1.IMC() >= 18.5 and calc1.IMC() <= 24.9:
+  print("peso normal!")
+elif calc1.IMC() >= 25.0 and calc1.IMC() <= 29.9:
+  print("excesso de peso!")
+elif calc1.IMC() >= 30.0 and calc1.IMC() <= 34.9:
+  print("obesidade classe I")
+elif calc1.IMC() >= 35.0 and calc1.IMC() <= 39.9:
+  print("obesidade classe II")
+elif calc1.IMC() >= 40 :
+  print("obesidade classe III")
+else:
+  print("dados errados")
