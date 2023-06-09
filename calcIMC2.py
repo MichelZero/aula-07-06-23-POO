8ef03c92d2e515191ab5e9951064ccbf2cbff73f
Michel <106674127+MichelZero@users.noreply.github.com>
2023-06-09 15:58:03 -0300

    up

--- calcIMC2.py
+++ calcIMC2.py
@@ -17,4 +17,24 @@ class CalculadoraIMC():
     
   def IMC(self):
     resultado = self.peso / self.altura ** 2
-    return resultado
+    # arredondando o resultado para duas casas decimais.
+    # chamando o método classificacao() dentro do método IMC()
+    return classificacao(self)
+  
+  # criando o método para retornar a classificação do IMC.
+  def classificacao(self):
+    if self.IMC() < 18.5:
+      return "abaixo do peso normal!"
+    elif self.IMC() >= 18.5 and self.IMC() <= 24.9:
+      return "peso normal!"
+    elif self.IMC() >= 25.0 and self.IMC() <= 29.9:
+      return "excesso de peso!"
+    elif self.IMC() >= 30.0 and self.IMC() <= 34.9:
+      return "obesidade classe I"
+    elif self.IMC() >= 35.0 and self.IMC() <= 39.9:
+      return "obesidade classe II"
+    elif self.IMC() >= 40 :
+      return "obesidade classe III"
+    else:
+      return "dados errados"
+    
--- main.py
+++ main.py
@@ -4,3 +4,16 @@
 #
 # data: 09/06/2023
 
+# importando a classe CalculadoraIMC
+from calcIMC import CalculadoraIMC as CIMC
+
+# criando o objeto CalculadoraIMC
+calc1 = CIMC()
+
+# entrada de dados
+peso = int(input("informe o peso: "))
+altura = float(input("informe a altura: "))
+#
+calc1.setPeso(peso)
+calc1.setAltura(altura)
+print(calc1.IMC())
